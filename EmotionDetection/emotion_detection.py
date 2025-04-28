10e48b6be4b218c78b5014b60411bf7947f00588
Dmytro K <clapandslap@gmail.com>
2025-04-28 14:49:43 -0400

    Implement EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+
+EMOTION_DETECTOR_URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+
+def emotion_detector(text_to_analyze) -> str:
+    body = { "raw_document": { "text": text_to_analyze } }
+    headers = __build_headers()
+    response = requests.post(EMOTION_DETECTOR_URL, headers=headers, json=body)
+    result = response.json()
+
+    emotions = result["emotionPredictions"][0]["emotion"]
+    emotions = {k: v for k, v in sorted(emotions.items(), key=lambda item: item[1], reverse=True)}
+    dominant_emotion = list(emotions.keys())[0]
+    return emotions | {"dominant_emotion": dominant_emotion}
+
+    
+def __build_headers() -> dict:
+    return {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+
